fcb2c210cc3a90f945081b112cb1c893027307a0
sakitayu <sakitakitakita@gmail.com>
2021-10-11 19:15:08 +0900

    Write a query

--- script.ts
+++ script.ts
@@ -4,7 +4,11 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  })
+  // use `console.dir` to print nested objects
+  console.dir(allUsers, { depth: null })
 }
 
 main()
